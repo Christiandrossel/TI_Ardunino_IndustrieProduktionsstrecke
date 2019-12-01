a9074a1222024e368a03d7b14e3e839882233c0f
Norman M <norman.moersch@gmail.com>
2019-12-01 23:05:10 +0100

    Code Cleanup, Commenting

--- Android Server App/app/src/main/java/de/htwdd/tiserver/IMachineCommunicator.java
+++ Android Server App/app/src/main/java/de/htwdd/tiserver/IMachineCommunicator.java
@@ -1,7 +1,24 @@
 package de.htwdd.tiserver;
 
+/**
+ * An interface for the communication between the different Bluetooth Clients
+ */
 public interface IMachineCommunicator {
+
+    /**
+     * Sets the specified Drill in Busy state
+     * @param drill the Drill ID (1-3)
+     */
     void setDrillBusy(int drill);
+
+    /**
+     * Sets the specified Drill in Free state
+     * @param drill the Drill ID (1-3)
+     */
     void setDrillFree(int drill);
+
+    /**
+     * @return returns the first free Drill
+     */
     int getFreeDrill();
 }
