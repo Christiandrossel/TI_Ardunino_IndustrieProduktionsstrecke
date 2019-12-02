8180194344433de6839b6a7dd0fb460e47f8a87e
ManuelSchilling <asuron@gmx.de>
2019-12-02 19:17:15 +0100

    Update linetracking_UltraSensor.ino
    add enum rodoterstate
    add Serial.print and Serial.read
    add switch statements for rodoterstate and server answer
    add function to recognize breakpoints

--- Rodoter/linetracking_UltraSensor/linetracking_UltraSensor.ino
+++ Rodoter/linetracking_UltraSensor/linetracking_UltraSensor.ino
@@ -1,10 +1,16 @@
-int MotorRight1=5;
-int MotorRight2=6; 
-int MotorLeft1=10;
-int MotorLeft2=11;
+//rechter Motor 
+int MotorRight1=5; //rückwärts
+int MotorRight2=6; // vorwärts
+//Linker Motor 
+int MotorLeft1=10; //rückwärts
+int MotorLeft2=11; // vorwärts
+
+// Ultrasensoren in Gestalt der Rodoteraugen 
 #define echoPin 12 // Echo Pin
 #define trigPin 13 // Trigger Pin
-long duration, distance; // Duration used to calculate distance
+long duration, distance; // Variablen für die Distanz
+
+// Linetracking Variablen
 const int SensorLeft = 7;  // pin for sensor on the left
 const int SensorMiddle= 4 ;// pin for sensor in the middle  
 const int SensorRight = 3;// pin for sensor on the right 
@@ -12,10 +18,16 @@ int SL; // left sensor status
 int SM; // middle sensor status 
 int SR; // right sensor status
 
-int server = 1;
+//Variable für die Werte vom Server
+char server; 
+// 1 or right for right direction
+// 2 or left for left direction 
+// 3 or middle for middle direction
+//Rodoterzustände
+enum rodoterstate {GO_TO_CRANE,GO_TO_CROSSROAD,GO_TO_DRILL};
+rodoterstate RodoterState = GO_TO_CRANE;
 
-void setup()
-{
+void setup(){
   Serial.begin(9600);
   pinMode(MotorRight1, OUTPUT); // pin  5 (PWM) 
   pinMode(MotorRight2, OUTPUT); // pin  6 (PWM) 
@@ -29,9 +41,10 @@ void setup()
 
 }
 
-
 void loop()
 {
+  server=Serial.read();
+
   SL = digitalRead(SensorLeft);
   SM = digitalRead(SensorMiddle);
   SR = digitalRead(SensorRight);
@@ -44,89 +57,125 @@ void loop()
   
   duration = pulseIn(echoPin, HIGH);
   distance = duration/58.2;
-  if(distance > 5)
-  {
-    if (SM == HIGH)// middle sensor in black area
-    {
-      if (SL == LOW & SR == HIGH) // black on left, white on right, turn left
-      {
-        digitalWrite(MotorRight1,LOW);
-        digitalWrite(MotorRight2,HIGH);
-        digitalWrite(MotorLeft1,LOW);
-        digitalWrite(MotorLeft2,LOW);
-      }
-      else if (SR == LOW & SL == HIGH) // white on left, black on right, turn right
-      {
-        digitalWrite(MotorLeft1,LOW);
-        digitalWrite(MotorLeft2,HIGH);
-        digitalWrite(MotorRight1,LOW);
-        digitalWrite(MotorRight2,LOW);
-      }
-      else // white on both sides, going forward
-      {
-        digitalWrite(MotorRight1,LOW);
-        digitalWrite(MotorRight2,HIGH);
-        digitalWrite(MotorLeft1,LOW);
-        digitalWrite(MotorLeft2,HIGH);
-      }
-    }
-    else // middle sensor on white area
-    {
-      if (SL== LOW & SR == HIGH)// black on left, white on right, turn left
-      {
-        digitalWrite(MotorRight1,LOW);
-        digitalWrite(MotorRight2,HIGH);
-        digitalWrite(MotorLeft1,LOW);
-        digitalWrite(MotorLeft2,LOW);
-      }
-      else if (SR == LOW & SL == HIGH) // white on left, black on right, turn right
-      {
-        digitalWrite(MotorLeft1,LOW);
-        digitalWrite(MotorLeft2,HIGH);
-        digitalWrite(MotorRight1,LOW);
-        digitalWrite(MotorRight2,LOW);
-      }
-      else // all white, stop
-      {
-        digitalWrite(MotorLeft1,LOW);
-        digitalWrite(MotorLeft2,LOW);
-        digitalWrite(MotorRight1,LOW);
-        digitalWrite(MotorRight2,LOW);
-        
-  // Serverwerte werden ausgewertet und Rodoter bewegt sich in entsprechender Richtung
   
-        if(server == 1){
-          digitalWrite(MotorLeft1,LOW);
-          digitalWrite(MotorLeft2,HIGH);
-          digitalWrite(MotorRight1,LOW);
-          digitalWrite(MotorRight2,LOW);
-          delay(1000);
-          server = 0;
-        }
-         if(server == 2){
-          digitalWrite(MotorLeft1,LOW);
-          digitalWrite(MotorLeft2,LOW);
-          digitalWrite(MotorRight1,LOW);
-          digitalWrite(MotorRight2,HIGH);
-          delay(1000);
-          server = 0;
-        }
-         if(server == 3){
-          digitalWrite(MotorLeft1,LOW);
-          digitalWrite(MotorLeft2,HIGH);
-          digitalWrite(MotorRight1,LOW);
-          digitalWrite(MotorRight2,HIGH);
-          delay(1000);
-          server = 0;
-        }
-      }
-    }
-  }else
-  {
+  if(distance > 4)
+    follow_line();
+  else{
     digitalWrite(MotorLeft1,LOW);
     digitalWrite(MotorLeft2,LOW);
     digitalWrite(MotorRight1,LOW);
     digitalWrite(MotorRight2,LOW);
   }
+  switch(RodoterState){
+    case GO_TO_CRANE:
+      if(breakpoint_recognized())
+        Serial.print('r');
+      if(server=='r'){
+        digitalWrite(MotorLeft1,LOW);
+        digitalWrite(MotorLeft2,HIGH);
+        digitalWrite(MotorRight1,LOW);
+        digitalWrite(MotorRight2,HIGH);
+        delay(1000);
+        RodoterState = GO_TO_CROSSROAD;
+      }
+      break;
+    case GO_TO_CROSSROAD:
+      if(breakpoint_recognized())
+        Serial.print('d');
+      switch(server){
+        case '0':
+        digitalWrite(MotorLeft1,LOW);
+        digitalWrite(MotorLeft2,LOW);
+        digitalWrite(MotorRight1,LOW);
+        digitalWrite(MotorRight2,LOW);
+        RodoterState = GO_TO_DRILL;
+        delay(1000);
+        break;
+        case '1':
+        digitalWrite(MotorLeft1,LOW); 
+        digitalWrite(MotorLeft2,HIGH);
+        digitalWrite(MotorRight1,LOW);
+        digitalWrite(MotorRight2,LOW);
+        RodoterState = GO_TO_DRILL;
+        delay(1000);
+        break;
+        case '2':
+        digitalWrite(MotorLeft1,LOW);
+        digitalWrite(MotorLeft2,LOW);
+        digitalWrite(MotorRight1,LOW);
+        digitalWrite(MotorRight2,HIGH);
+        RodoterState = GO_TO_DRILL;
+        delay(1000);
+        break;
+      }
+      break; 
+    case GO_TO_DRILL:
+      if(breakpoint_recognized())
+        Serial.print('r');
+      if(server=='r'){
+        digitalWrite(MotorLeft1,LOW);
+        digitalWrite(MotorLeft2,HIGH);
+        digitalWrite(MotorRight1,LOW);
+        digitalWrite(MotorRight2,HIGH);
+        delay(1000);
+        RodoterState = GO_TO_CRANE;
+      }
+      break;
+    }
 }
- 
+void follow_line(){
+  if (SM == HIGH)// middle sensor in black area
+  {
+    if (SL == LOW & SR == HIGH) // black on left, white on right, turn left
+    {
+      digitalWrite(MotorRight1,LOW);
+      digitalWrite(MotorRight2,HIGH);
+      digitalWrite(MotorLeft1,LOW);
+      digitalWrite(MotorLeft2,LOW);
+    }
+    else if (SR == LOW & SL == HIGH) // white on left, black on right, turn right
+    {
+      digitalWrite(MotorLeft1,LOW);
+      digitalWrite(MotorLeft2,HIGH);
+      digitalWrite(MotorRight1,LOW);
+      digitalWrite(MotorRight2,LOW);
+    }
+    else // white on both sides, going forward
+    {
+      digitalWrite(MotorRight1,LOW);
+      digitalWrite(MotorRight2,HIGH);
+      digitalWrite(MotorLeft1,LOW);
+      digitalWrite(MotorLeft2,HIGH);
+    }
+  }
+  else // middle sensor on white area
+  {
+    if (SL== LOW & SR == HIGH)// black on left, white on right, turn left
+    {
+      digitalWrite(MotorRight1,LOW);
+      digitalWrite(MotorRight2,HIGH);
+      digitalWrite(MotorLeft1,LOW);
+      digitalWrite(MotorLeft2,LOW);
+    }
+    else if (SR == LOW & SL == HIGH) // white on left, black on right, turn right
+    {
+      digitalWrite(MotorLeft1,LOW);
+      digitalWrite(MotorLeft2,HIGH);
+      digitalWrite(MotorRight1,LOW);
+      digitalWrite(MotorRight2,LOW);
+    }
+    else // all white, stop
+    {
+      digitalWrite(MotorLeft1,LOW);
+      digitalWrite(MotorLeft2,LOW);
+      digitalWrite(MotorRight1,LOW);
+      digitalWrite(MotorRight2,LOW);
+    }
+  }
+} 
+boolean breakpoint_recognized(){
+  if(SM==LOW && SR==LOW && SL==LOW)
+    return true;
+  else
+    return false;
+}
