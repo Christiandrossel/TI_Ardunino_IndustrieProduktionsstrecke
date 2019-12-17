9fa3b5870b2f2bb1ecf1b88597a17c24b5f36af2
s75969 <s75969@htw-dresden.de>
2019-12-17 23:34:56 +0100

    Integrate lcd and adjust server communication...
    Currently changes into busy mode after server message. No light resistor
    implemented.

--- Bohrer/bohrer/bohrer.ino
+++ Bohrer/bohrer/bohrer.ino
@@ -1,39 +1,75 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(11, 12, 2, 3, 4, 5);
 enum state {BUSY, FREE};
 state State = FREE;
-char message; //char that stores the incoming message
+bool reportState = false;
+//total working time in seconds
+int duration = 5;
+int timeElapsed = 0;
+// full bar charackter
+byte bar[8]={
+  B1111,
+  B1111,
+  B1111,
+  B1111,
+  B1111,
+  B1111,
+  B1111,
+  B1111
+};
+
+void printReady(){
+    lcd.clear();
+    lcd.print("Bereit");
+}
+
+void printWorking(){
+    int barSize = (16 * timeElapsed)/duration;
+    lcd.clear();
+    lcd.print("Fortschritt:");
+    lcd.setCursor(0,1);
+    for (int i = 0; i < barSize; i++){
+      lcd.write(byte(0));
+    }
+}
 
 void setup()
 {
  // pinMode(4,INPUT); 
  // Serial.begin(38400); // Default communication rate of the Bluetooth module
  Serial.begin(9600);  //set Baud rate
+  // set display size
+  lcd.begin(16, 2);
+  // create custom char bar for lcd
+  lcd.createChar(0,bar);
 }
 
 void loop(){
 
- if(Serial.available() > 0){ // Checks whether data is comming from the serial port
-    message = Serial.read(); // Reads the data from the serial port
-    Serial.print(message); //sends message back, acknowledgement of receipt
- 
- 
-    if(message=='f') //set state to free when a f was received
-    {
-      State = FREE;
-    }
-    if(message=='r') //set state to busy for a while if an r was received
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    if(Serial.read()=='b') //set state to busy
     {
       State = BUSY;
-          delay(5000); //simulate working time...
-      //delay(random(10000,20000));
-      State = FREE;
     }
-    if (message!='f'&&message!='b'&&message!='r') //should not occur
-    {// Do something if the message doesn't match the expectations
-    }
-    
-  delay(500); //delay
-  
  }
-
+  if(State == FREE){
+    if (!reportState){
+      Serial.print('r');
+      reportState = true;
+    }
+    delay(500);
+    printReady();
+  }else if (State == BUSY){
+    printWorking();
+    delay(1000);
+    timeElapsed++;
+    if (timeElapsed >= duration){
+      printWorking();
+      State = FREE;
+      timeElapsed = 0;
+      reportState = false;
+    }
+  }
 }
     
